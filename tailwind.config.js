52fea2af1cff40f321b600e90781a4562680186b
nigelwhite <webdev@permaculture.org.uk>
2021-02-21 18:52:40 +0000

    loads a changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
 module.exports = {
-  purge: {
-    mode: "all",
-    content: ["./**/*.html"],
-    options: {
-      whitelist: [],
-    },
-  },
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      colors: {},
-    },
-  },
-  variants: {},
-  plugins: [require("@tailwindcss/typography")],
+	purge: {
+		mode: 'all',
+		content: ['./**/*.html'],
+		options: {
+			whitelist: [],
+		},
+	},
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {},
+		},
+		debugScreens: {
+			position: ['top', 'left'],
+		},
+		variants: {},
+		plugins: [
+			require('@tailwindcss/typography'),
+			require('tailwindcss-debug-screens'),
+		],
+	},
 };
